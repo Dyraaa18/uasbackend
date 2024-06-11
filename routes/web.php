6add72ff8f34db3c05171e98da72268ec2d71f32
Dyraaa18 <adang.535220192@stu.untar.ac.id>
2024-06-11 23:44:16 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -61,6 +61,9 @@ Route::middleware(['auth:admin'])->group(function () {
     Route::get('/admin/dashboard', function () {
         return view('admin.dashboard');
     })->name('admin.dashboard');
+});
+
+Route::middleware(['guest:admin'])->group(function () {
     Route::get('admin/login', [AuthenticatedSessionController::class, 'create'])->name('admin.login');
     Route::post('admin/login', [AuthenticatedSessionController::class, 'store']);
 });
