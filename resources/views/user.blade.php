dfc54eeeeead87b4e2d1681caa6d98b26af793d6
amosnbbn <amosvalentino95@gmail.com>
2024-06-11 22:09:02 +0700

    css buat user
    asa

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Profile</title>
+    <link rel="stylesheet" href="{{ asset('css/user.css') }}">
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
 </head>
 <body>
     @include('layouts.navbar')
 
-    <div>
+    <div class="user-container">
         <p><strong>Username:</strong> {{ Auth::user()->name }}</p>
         <p><strong>Email:</strong> {{ Auth::user()->email }}</p>
         <!-- Tambahkan bidang lain dari profil pengguna di sini -->
