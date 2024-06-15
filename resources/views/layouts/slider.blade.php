984ba627545d26b429e9c1b6877a8259b404c6e6
danielhartonohonoris <daniel.535220206@stu.untar.ac.id>
2024-06-15 13:09:58 +0700

    Membuat tampilan ui ux LOGIN DAN REGISTER
    menyelesaikan login dan register

--- resources/views/layouts/slider.blade.php
+++ resources/views/layouts/slider.blade.php
@@ -1,90 +1,81 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/slider.css') }}">
     <link rel="stylesheet" href="{{ asset('css/variables.css') }}">
-    <script src="{{ asset('js/slider.js') }}"></script>
-    <link rel="bootstrap-5.0.2-dist/css/bootstrap.css" rel="stylesheet">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <!-- Google Fonts -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;500&family=Inter:wght@400;500&family=Playfair+Display:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+    <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/vendor/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/vendor/glightbox/css/glightbox.min.css">
+    <link rel="stylesheet" href="assets/vendor/aos/aos.css">
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;500&family=Inter:wght@400;500&family=Playfair+Display:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 </head>
+<body>
 
 <!-- ======= Hero Slider Section ======= -->
- <body>
 <section id="hero-slider" class="hero-slider">
-      <div class="container-md" data-aos="fade-in">
+    <div class="container-md" data-aos="fade-in">
         <div class="row">
-          <div class="col-12">
-            <div class="swiper sliderFeaturedPosts">
-              <div class="swiper-wrapper">
-                <div class="swiper-slide">
-                  <a href="https://www.alodokter.com/9-kandungan-rokok-yang-berefek-mengerikan-untuk-tubuh" class="img-bg d-flex align-items-end" style="background-image: url('https://res.cloudinary.com/dk0z4ums3/image/upload/v1605520828/attached_image/9-kandungan-rokok-yang-berefek-mengerikan-untuk-tubuh-0-alodokter.jpg');">
-                    <div class="img-bg-inner">
-                      <h2>9 Kandungan Rokok yang Berbahaya bagi Kesehatan Tubuh</h2>
-                      <p>Kandungan rokok terdiri dari berbagai macam zat kimia, termasuk nikotin yang bisa membahayakan kesehatan tubuh. Selain itu, di dalam asap rokok, setidaknya ada 70 bahan kimia yang bersifat racun bagi tubuh dan dapat memicu gangguan kesehatan. 
-
-Kandungan rokok yang bersifat racun tersebut berpotensi merusak sel-sel tubuh. Selain itu, senyawa dalam asap rokok juga bersifat karsinogenik atau memicu kanker. Di dalam rokok terdapat 250 jenis zat beracun dan 70 jenis zat yang diketahui bersifat karsinogenik.</p>
+            <div class="col-12">
+                <div class="swiper sliderFeaturedPosts">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <a href="https://www.alodokter.com/9-kandungan-rokok-yang-berefek-mengerikan-untuk-tubuh" class="img-bg" style="background-image: url('https://res.cloudinary.com/dk0z4ums3/image/upload/v1605520828/attached_image/9-kandungan-rokok-yang-berefek-mengerikan-untuk-tubuh-0-alodokter.jpg');">
+                                <div class="img-bg-inner">
+                                    <h2>9 Kandungan Rokok yang Berbahaya bagi Kesehatan Tubuh</h2>
+                                    <p>Kandungan rokok terdiri dari berbagai macam zat kimia, termasuk nikotin yang bisa membahayakan kesehatan tubuh...</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="swiper-slide">
+                            <a href="https://www.alodokter.com/6-ciri-ciri-kelebihan-minum-air-putih-yang-perlu-dikenali" class="img-bg" style="background-image: url('https://res.cloudinary.com/dk0z4ums3/image/upload/v1718009619/attached_image/6-ciri-ciri-kelebihan-minum-air-putih-yang-perlu-dikenali-0-alodokter.jpg');">
+                                <div class="img-bg-inner">
+                                    <h2>6 Ciri-Ciri Kelebihan Minum Air Putih yang Perlu Dikenali</h2>
+                                    <p>Pada umumnya, anjuran untuk minum air putih dalam sehari adalah setidaknya 8 gelas atau 2 liter per hari...</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="swiper-slide">
+                            <a href="https://www.alodokter.com/10-jus-yang-bagus-untuk-kulit" class="img-bg" style="background-image: url('https://res.cloudinary.com/dk0z4ums3/image/upload/v1717661983/attached_image/10-jus-yang-bagus-untuk-kulit-0-alodokter.jpg');">
+                                <div class="img-bg-inner">
+                                    <h2>10 Jus yang Bagus untuk Kulit</h2>
+                                    <p>Jus yang bagus untuk kulit umumnya mengandung nutrisi, seperti vitamin A, vitamin B, vitamin C, vitamin E, mineral...</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="swiper-slide">
+                            <a href="https://www.alodokter.com/sakit-kepala-sebelah-kiri-berdenyut-ini-7-penyebab-dan-cara-mengatasinya" class="img-bg" style="background-image: url('https://res.cloudinary.com/dk0z4ums3/image/upload/v1717368888/attached_image/sakit-kepala-sebelah-kiri-berdenyut-ini-7-penyebab-dan-cara-mengatasinya-0-alodokter.jpg');">
+                                <div class="img-bg-inner">
+                                    <h2>Sakit Kepala Sebelah Kiri Berdenyut, Ini 7 Penyebab dan Cara Mengatasinya</h2>
+                                    <p>Sakit kepala sebelah kiri berdenyut umumnya dapat hilang dengan sendirinya, tetapi jangan pula disepelekan...</p>
+                                </div>
+                            </a>
+                        </div>
                     </div>
-                  </a>
-                </div>
-
-                <div class="swiper-slide">
-                  <a href="https://www.alodokter.com/6-ciri-ciri-kelebihan-minum-air-putih-yang-perlu-dikenali" class="img-bg d-flex align-items-end" style="background-image: url('https://res.cloudinary.com/dk0z4ums3/image/upload/v1718009619/attached_image/6-ciri-ciri-kelebihan-minum-air-putih-yang-perlu-dikenali-0-alodokter.jpg');">
-                    <div class="img-bg-inner">
-                      <h2>6 Ciri-Ciri Kelebihan Minum Air Putih yang Perlu Dikenali</h2>
-                      <p>Pada umumnya, anjuran untuk minum air putih dalam sehari adalah setidaknya 8 gelas atau 2 liter per hari. Ini karena air merupakan komponen utama tubuh manusia yang membentuk sebagian besar sel, jaringan, maupun organ dalam tubuh. Selain itu, air juga memiliki banyak manfaat untuk mendukung kesehatan, sehingga sangat diperlukan oleh tubuh.</p>
+                    <div class="custom-swiper-button-next">
+                        <span class="bi-chevron-right"></span>
                     </div>
-                  </a>
-                </div>
-
-                <div class="swiper-slide">
-                  <a href="https://www.alodokter.com/10-jus-yang-bagus-untuk-kulit" class="img-bg d-flex align-items-end" style="background-image: url('https://res.cloudinary.com/dk0z4ums3/image/upload/v1717661983/attached_image/10-jus-yang-bagus-untuk-kulit-0-alodokter.jpg');">
-                    <div class="img-bg-inner">
-                      <h2>10 Jus yang Bagus untuk Kulit</h2>
-                      <p>Jus yang bagus untuk kulit umumnya mengandung nutrisi, seperti vitamin A, vitamin B, vitamin C, vitamin E, mineral, dan juga antioksidan. Rutin mengonsumsi jus ini bisa membuat kulit tampak lebih cerah, kenyal, dan awet muda, lho.
-                        Ada banyak pilihan jus yang bagus untuk kulit, seperti jus tomat, jus jeruk, jus kiwi, jus alpukat, sampai jus stroberi. Beragam jus tersebut mengandung nutrisi yang bisa melindungi kulit dari kerusakan akibat paparan radikal bebas, polusi, dan paparan sinar matahari berlebihan yang juga merupakan penyebab kulit kering dan cepat menua.</p>
+                    <div class="custom-swiper-button-prev">
+                        <span class="bi-chevron-left"></span>
                     </div>
-                  </a>
+                    <div class="swiper-pagination"></div>
                 </div>
-
-                <div class="swiper-slide">
-                  <a href="https://www.alodokter.com/sakit-kepala-sebelah-kiri-berdenyut-ini-7-penyebab-dan-cara-mengatasinya" class="img-bg d-flex align-items-end" style="background-image: url('https://res.cloudinary.com/dk0z4ums3/image/upload/v1717368888/attached_image/sakit-kepala-sebelah-kiri-berdenyut-ini-7-penyebab-dan-cara-mengatasinya-0-alodokter.jpg');">
-                    <div class="img-bg-inner">
-                      <h2>Sakit Kepala Sebelah Kiri Berdenyut, Ini 7 Penyebab dan Cara Mengatasinya</h2>
-                      <p>Sakit kepala sebelah kiri berdenyut umumnya dapat hilang dengan sendirinya, tetapi jangan pula disepelekan. Pasalnya, selain dapat mengganggu aktivitas, kondisi ini mungkin merupakan gejala dari penyakit tertentu, seperti tekanan darah tinggi. 
-                      Sakit kepala berdenyut timbul karena adanya pelebaran pembuluh darah akibat meningkatnya aliran darah ke kepala. Kondisi ini bisa menyebabkan rasa sakit pada seluruh bagian kepala atau satu sisi saja, contohnya pada sisi kepala sebelah kiri.   </p>
-                    </div>
-                  </a>
-                </div>
-              </div>
-              <div class="custom-swiper-button-next">
-                <span class="bi-chevron-right"></span>
-              </div>
-              <div class="custom-swiper-button-prev">
-                <span class="bi-chevron-left"></span>
-              </div>
-
-              <div class="swiper-pagination"></div>
-              
             </div>
-          </div>
         </div>
-      </div>
-    </section><!-- End Hero Slider Section -->
-    <script src="{{ asset('js/slider.js') }}"></script>
+    </div>
+</section><!-- End Hero Slider Section -->
+
+<!-- Search Form -->
+<div class="search-form-wrap js-search-form-wrap">
+</div>
 </body>
+</html>
