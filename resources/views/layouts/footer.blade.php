138d30e12c8bb324a185ac7f01963b6f6e05b925
danielhartonohonoris <daniel.535220206@stu.untar.ac.id>
2024-06-27 00:15:39 +0700

    Home Update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -30,7 +30,7 @@
           <ion-icon name="logo-instagram"></ion-icon>
         </a></li>
     </ul>
-    <p>&copy;2024 Klinik Hartono Medica</p>
+    <p>&copy;2024 Klinik Hartono Medika</p>
   </footer>
   <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
